bb080f40233b984377914062d75ac63879fcff87
somin <salth0601@gmail.com>
2023-12-14 23:26:46 +0900

    test: 예외 테스트 코드 추가

--- src/test/kotlin/pairmatching/ApplicationTest.kt
+++ src/test/kotlin/pairmatching/ApplicationTest.kt
@@ -17,6 +17,39 @@ internal class ApplicationTest : NsTest() {
             listOf("태웅", "백호", "치수", "태섭")
         )
     }
+    @Test
+    fun `홀수_인원_페어_매칭`() {
+        assertShuffleTest(
+            {
+                run("1", "프론트엔드, 레벨1, 자동차경주", "Q")
+                assertThat(output()).contains("보노 : 시저", "쉐리 : 신디 : 다비")
+            },
+            listOf("보노", "시저", "쉐리", "신디", "다비")
+        )
+    }
+
+    @Test
+    fun `없는_커멘드에_대한_예외_처리`() {
+        assertSimpleTest {
+            runException("0", "백엔, 레벨1, 로또")
+            assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+    @Test
+    fun `없는_코스에_대한_예외_처리`() {
+        assertSimpleTest {
+            runException("1", "안드로이드, 레벨1, 로또")
+            assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+
+    @Test
+    fun `없는_레벨에_대한_예외_처리`() {
+        assertSimpleTest {
+            runException("1", "백엔드, 레벨6, 로또")
+            assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
 
     @Test
     fun `없는_미션에_대한_예외_처리`() {
